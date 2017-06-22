523a1fdaf915575dbd9fa3526ab3a391baaadcb4
chan <chanjobs21252@gmail.com>
2017-06-22 18:46:21 +0800

    微信接口测试

--- wechat.py
+++ wechat.py
@@ -1,27 +1,9 @@
 #！Python3
-#coidng=utf8
+#*--coidng=utf8--*
 
 import itchat
-from itchat.content import *
-
-@itchat.msg_register([TEXT, MAP, CARD, NOTE, SHARING])
-def text_reply(msg):
-    itchat.send('%s: %s' % (msg['Type'], msg['Text']), msg['FromUserName'])
-
-@itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO])
-def download_files(msg):
-    msg['Text'](msg['FileName'])
-    return '@%s@%s' % ({'Picture': 'img', 'Video': 'vid'}.get(msg['Type'], 'fil'), msg['FileName'])
-
-@itchat.msg_register(FRIENDS)
-def add_friend(msg):
-    itchat.add_friend(**msg['Text']) # 该操作会自动将新好友的消息录入，不需要重载通讯录
-    itchat.send_msg('Nice to meet you!', msg['RecommendInfo']['UserName'])
-
-@itchat.msg_register(TEXT, isGroupChat=True)
-def text_reply(msg):
-    if msg['isAt']:
-        itchat.send(u'@%s\u2005I received: %s' % (msg['ActualNickName'], msg['Content']), msg['FromUserName'])
+#from itchat.content import *
 
 itchat.auto_login()
-itchat.run()
+user = itchat.search_friends(name=u'官宾宾')[0]
+user.send(u'这是一条测试信息，请忽略')
